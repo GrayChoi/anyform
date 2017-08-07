f4880f0972bac7453364c0b72d8808a280a8b72a
GrayChoi <gray.choi.1988@gmail.com>
2017-08-07 21:48:53 +0900

    :package: cors

--- functions/dev.js
+++ functions/dev.js
@@ -4,18 +4,8 @@ const express = require('express');
 const morgan = require('morgan');
 const { api } = require('./index.js');
 
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
-
-    next();
-}
-
 const app = express();
 
-app.use(allowCrossDomain);
 app.use(morgan('dev', { immediate: true }));
 app.options('*', (req, res) => {
     res.status(200).send();
