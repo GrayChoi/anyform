4200ceb4f71433e46b855de42c4b3aa90ef0bf23
GrayChoi <gray.choi.1988@gmail.com>
2017-08-07 22:07:49 +0900

    cors

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,20 @@
 const functions = require('firebase-functions');
 const bodyParser = require('body-parser');
 const express = require('express');
-const cors = require('cors')({ origin: true });
+const cors = require('cors')({ 
+  origin: 'https://anyform-e1843.firebaseapp.com',
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+});
 const auth = require('./middlewares/auth')
 const controllers = require('./controllers');
 
 const app = new express();
 
+app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(auth);
 
-app.use(cors);
 app.use(controllers);
 
 exports.api = functions.https.onRequest(app);
